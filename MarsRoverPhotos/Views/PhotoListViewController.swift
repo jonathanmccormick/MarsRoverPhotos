9583e95e34af616ddcd7f68044b5997834c8c2f9
Jonathan McCormick <jonathanimccormick@gmail.com>
2020-05-27 06:27:57 -0600

    Organize

--- MarsRoverPhotos/Views/PhotoListViewController.swift
+++ MarsRoverPhotos/Views/PhotoListViewController.swift
@@ -11,36 +11,55 @@ import Alamofire
 import AlamofireImage
 
 class PhotoListViewController: UIViewController {
+    
+    // MARK: Outlets
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    public var rover: RoverModel!
+    // MARK: Public ID
     public static let id = "PhotoListViewController"
+
+    // MARK: Public rover property
+    public var rover: RoverModel!
+    
+    // MARK: Private data
     private var photos: [PhotoDTO] = []
     private var currentPage = 1
     private var loadingData = false
     
+    // MARK: Lifecycle overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = rover.name
-        tableView.isHidden = true
-        activityIndicator.isHidden = false
-        
-        let nib = UINib(nibName: "ImageTableViewCell", bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: "ImageTableViewCell")
+        showLoadingIndicator()
+        registerNibForTableViewCell()
         loadData()
     }
     
+    // MARK: Private logic
+    private func registerNibForTableViewCell() {
+        let nib = UINib(nibName: ImageTableViewCell.id, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: ImageTableViewCell.id)
+    }
+    
+    private func showLoadingIndicator() {
+        tableView.isHidden = true
+        activityIndicator.isHidden = false
+    }
+    
+    private func hideLoadingIndicator() {
+        self.tableView.isHidden = false
+        self.activityIndicator.isHidden = true
+    }
+    
     private func loadData() {
-        
         print(currentPage)
         EndpointManager.sharedInstance.getPhotos(rover: rover, page: currentPage) {
             photos in
             self.photos.append(contentsOf: photos.map{ $0.toDTO() })
             self.tableView.reloadData()
-            self.tableView.isHidden = false
-            self.activityIndicator.isHidden = true
+            self.hideLoadingIndicator()
             self.currentPage += 1
             self.loadingData = false
         }
@@ -49,44 +68,44 @@ class PhotoListViewController: UIViewController {
 
 extension PhotoListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return photos.count
-        }
+        return photos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImageTableViewCell.id) as! ImageTableViewCell
         
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: ImageTableViewCell.id) as! ImageTableViewCell
-            
-            if (photos[indexPath.row].image == nil) {
-                if let http = photos[indexPath.row].img_src {
-                    // Convert to https
-                    let https = "https" + http.dropFirst(4)
-                    
-                    AF.request(https).responseImage { response in
-                        if case .success(let image) = response.result {
-                            self.photos[indexPath.row].image = image
-                            cell.marsImage?.image = image
-                            self.tableView.reloadData()
-                        } else {
-                            print("error")
-                        }
+        if (photos[indexPath.row].image == nil) {
+            if let http = photos[indexPath.row].img_src {
+                // Convert to https
+                let https = "https" + http.dropFirst(4)
+                
+                AF.request(https).responseImage { response in
+                    if case .success(let image) = response.result {
+                        self.photos[indexPath.row].image = image
+                        cell.marsImage?.image = image
+                        self.tableView.reloadData()
+                    } else {
+                        print("error")
                     }
                 }
             }
-            
-            cell.marsImage?.image = photos[indexPath.row].image
-            cell.cameraNameLabel.text = photos[indexPath.row].camera?.full_name
-            cell.sol.text = "Sol \(photos[indexPath.row].sol!) (\(photos[indexPath.row].earth_date!))"
-            
-            return cell
         }
         
-        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-            if let image = photos[indexPath.row].image {
-                let ratio = image.size.height / image.size.width
-                return tableView.visibleSize.width / ratio
-            } else {
-                return 0
-            }
+        cell.marsImage?.image = photos[indexPath.row].image
+        cell.cameraNameLabel.text = photos[indexPath.row].camera?.full_name
+        cell.sol.text = "Sol \(photos[indexPath.row].sol!) (\(photos[indexPath.row].earth_date!))"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if let image = photos[indexPath.row].image {
+            let ratio = image.size.height / image.size.width
+            return tableView.visibleSize.width / ratio
+        } else {
+            return 0
         }
+    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)

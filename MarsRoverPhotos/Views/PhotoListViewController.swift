64bdd0e1780fecd4afd5a2cfc6b86b74aff9a2ed
Jonathan McCormick <jonathanimccormick@gmail.com>
2020-05-26 18:41:39 -0600

    Move UITableView stuff to extension

--- MarsRoverPhotos/Views/PhotoListViewController.swift
+++ MarsRoverPhotos/Views/PhotoListViewController.swift
@@ -10,46 +10,10 @@ import UIKit
 import Alamofire
 import AlamofireImage
 
-class PhotoListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+class PhotoListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     dynamic var list: [PhotoDTO] = []
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ImageTableViewCell.id) as! ImageTableViewCell
-        
-        if (list[indexPath.row].image == nil) {
-            if let http = list[indexPath.row].img_src {
-                // Convert to https
-                let https = "https" + http.dropFirst(4)
-                
-                AF.request(https).responseImage { response in
-                    if case .success(let image) = response.result {
-                        self.list[indexPath.row].image = image
-                        cell.marsImage?.image = image
-                    } else {
-                        print("error")
-                    }
-                }
-            }
-        }
-        
-        cell.marsImage?.image = list[indexPath.row].image
-        
-        return cell
-    }
-    
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        let image = list[indexPath.row].image!
-//        let ratio = image.size.height / image.size.width
-//        
-//        return tableView.visibleSize.width / ratio
-//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,6 +24,43 @@ class PhotoListViewController: UIViewController, UITableViewDelegate, UITableVie
             self.tableView.reloadData()
         }
     }
+}
+
+extension PhotoListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return list.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: ImageTableViewCell.id) as! ImageTableViewCell
+            
+            if (list[indexPath.row].image == nil) {
+                if let http = list[indexPath.row].img_src {
+                    // Convert to https
+                    let https = "https" + http.dropFirst(4)
+                    
+                    AF.request(https).responseImage { response in
+                        if case .success(let image) = response.result {
+                            self.list[indexPath.row].image = image
+                            cell.marsImage?.image = image
+                        } else {
+                            print("error")
+                        }
+                    }
+                }
+            }
+            
+            cell.marsImage?.image = list[indexPath.row].image
+            
+            return cell
+        }
+        
+    //    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    //        let image = list[indexPath.row].image!
+    //        let ratio = image.size.height / image.size.width
+    //
+    //        return tableView.visibleSize.width / ratio
+    //    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)

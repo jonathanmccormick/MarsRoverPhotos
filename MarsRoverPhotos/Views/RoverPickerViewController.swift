39ccff030f4ae327cbabd369046ac91a0bc77ba3
Jonathan McCormick <jonathanimccormick@gmail.com>
2020-05-27 06:03:40 -0600

    Organize viewcontroller

--- MarsRoverPhotos/Views/RoverPickerViewController.swift
+++ MarsRoverPhotos/Views/RoverPickerViewController.swift
@@ -9,33 +9,53 @@
 import Foundation
 import UIKit
 
-class RoverPickerViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class RoverPickerViewController: UIViewController {
+    
+    // MARK: Outlets
     @IBOutlet weak var tableView: UITableView!
     
-    var rovers: [RoverModel] = []
+    // MARK: Variables
+    private var rovers: [RoverModel] = []
     
+    // MARK: Lifecycle Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loadRovers()
+    }
+    
+    // MARK: Private logic
+    private func loadRovers() {
         EndpointManager.sharedInstance.getRovers {
             rovers in
             self.rovers = rovers
             self.tableView.reloadData()
-            print(rovers)
         }
     }
-    
+}
+
+// MARK: - UITableViewDelegate
+extension RoverPickerViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         rovers.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         cell.textLabel?.text = rovers[indexPath.row].name
         return cell
     }
     
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        
-//    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let viewController = storyboard?.instantiateViewController(identifier: PhotoListViewController.id) as! PhotoListViewController
+        viewController.rover = rovers[indexPath.row]
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension RoverPickerViewController: UITableViewDataSource {
+    
 }
